78688913b9a9015ace0e9f31abf04c0b2d8026cc
LNN <1738458669@qqcom>
2021-07-22 18:19:10 +0800

    新增

--- src/java0720/Cat.java
+++ src/java0720/Cat.java
@@ -3,15 +3,26 @@ package java0720;
 public class Cat {
     public String name;
     public String gender;
+    static{
+        //静态代码块只在程序启动（更准确的说是类加载的时候）执行一次
+        System.out.println("这是一个静态代码块");
+    }
+
+    {
+        System.out.println("这是一个普通代码块");
+    }
+
+
 
     public Cat(String name, String gender) {//构造方法
         this.name = name;
         this.gender = gender;
-        this.eat("🐟");
+        //this.eat("🐟");
     }
-    public void eat(String food){
-        System.out.println(name +"正在吃" + food);
-    }
+//    public void eat(String food){
+//        System.out.println(name +"正在吃" + food);
+//    }
+    @Override
     public String toString(){
         return this.name + " , " + this.gender;
     }
--- src/java0720/Main.java
+++ src/java0720/Main.java
@@ -3,6 +3,7 @@ package java0720;
 public class Main {
     public static void main(String[] args) {
         Cat cat = new Cat("小黑","公猫");
-        System.out.println(cat);
+        Cat cat2 = new Cat("咪咪","母猫");
+        //System.out.println(cat);
     }
 }
